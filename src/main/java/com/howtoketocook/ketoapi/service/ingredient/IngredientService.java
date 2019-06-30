40852b72b848f91ce5016090bcc66af00db827cd
Paloma Schkrab <paloma.schkrab@gmail.com>
2019-06-30 12:23:32 -0700

    Changed IngredientService getIngredientByName method

--- src/main/java/com/howtoketocook/ketoapi/service/ingredient/IngredientService.java
+++ src/main/java/com/howtoketocook/ketoapi/service/ingredient/IngredientService.java
@@ -1,5 +1,8 @@
 package com.howtoketocook.ketoapi.service.ingredient;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,12 @@ public class IngredientService {
 	@Autowired
 	private IngredientRepository ingredientRepository;
 	
-	public Ingredient getIngredientByName(String name) {	
-		return ingredientRepository.findByNameIgnoreCase(name).orElseThrow();
+	public Ingredient getIngredientByName(String name) {
+		Optional<Ingredient> ingredient = ingredientRepository.findByNameIgnoreCase(name);
+		if(ingredient.isPresent()) {
+			return ingredient.get();
+		}
+		throw new NoSuchElementException("No value present");
 	}
 	
 	public Ingredient getIngredientById(long id) {	
