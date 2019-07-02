af5313c043129b4896850cf7fd745f50693e39a1
Paloma Schkrab <paloma.schkrab@gmail.com>
2019-07-01 18:20:03 -0700

    HTKC-132: Fixed recipe ingredients displaying 0/0

--- src/main/java/com/howtoketocook/ketoapi/converter/QuantityConverter.java
+++ src/main/java/com/howtoketocook/ketoapi/converter/QuantityConverter.java
@@ -4,12 +4,15 @@ public class QuantityConverter {
 
 	public static String convertFractionToString(int whole, int numerator, int denominator) {
 		if(whole == 0) {
-			return numerator + "/" + denominator;
+			if(numerator !=0) {
+				return numerator + "/" + denominator;
+			}
+			return "";
 		}
 		
 		if(numerator == 0) {
 			return String.valueOf(whole);
-		}
+		}		
 		
 		return whole + " " + numerator + "/" + denominator;
 	}
